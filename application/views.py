b46158637c3517175a32f02abfc48c01aa72df3e
arunjo5 <arunjo528@gmail.com>
2023-07-13 19:16:49 -0400

    first commit

---
+++ application/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+
+
+def home(request):
+    import json
+    import requests
+    if request.method == 'POST':
+        query = request.POST['query']
+        api_url = 'https://api.api-ninjas.com/v1/nutrition?query='
+        api_request = requests.get(
+            api_url + query, headers={'X-Api-Key': 'ul78Zb4YTN3yBW2P138gXMlHpylG1apiNzElNYrH'})
+        try:
+            api = json.loads(api_request.content)
+            print(api_request.content)
+        except Exception as e:
+            api = "oops! There was an error"
+            print(e)
+        return render(request, 'home.html', {'api': api})
+    else:
+        return render(request, 'home.html', {'query': 'Enter a valid query'})
+
+
